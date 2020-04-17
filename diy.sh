cb76e10c2764c9771868c0d6084fe1c339ee58c1
ssasss <33904125+ssasss@users.noreply.github.com>
2020-04-18 00:24:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,4 +8,3 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
 git clone https://github.com/vernesong/OpenClash package/OpenClash
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
