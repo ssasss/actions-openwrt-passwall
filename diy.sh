7c325aa0809fb43676ab29c26b2a00f28462e480
ssasss <33904125+ssasss@users.noreply.github.com>
2020-04-18 11:08:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
 git clone https://github.com/vernesong/OpenClash package/OpenClash
+git clone https://github.com/maxlicheng/luci-app-ssr-plus package/luci-app-ssr-plus
+git clone https://github.com/TinyTitanPro/lienol-openwrt-package package/passwall
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/AdguardTeam/AdGuardHome package/AdGuardHome
